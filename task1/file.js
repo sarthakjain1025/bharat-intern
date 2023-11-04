1e18fbedfae4869c7f6dd55f449236bbba235cd0
Sartak_Jain <sarthakjain206@gmail.com>
2023-11-04 23:24:31 +0530

    m1

---
+++ task1/file.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Java developer","Frontend Developer", "Web developer",  "C/C++ programmer"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop:true
+});
